873ca4362f4eb1f24e4a5ec9a9914ea0a36ba9d3
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-02-21 08:05:54 -0800

    Add files via upload

---
+++ JS/flow.js
@@ -0,0 +1,9 @@
+document.getElementById("btn01").onclick=
+  function (){
+    window.scrollTo(0, 1300);
+  }
+
+document.getElementById("btn03").onclick= 
+  function(){
+    window.scrollTo(0, 0);
+  }
